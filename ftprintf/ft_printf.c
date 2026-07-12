56083a4b445f6f8b2b1bb9d5abe36efd4d040dcb
gusthsb <gustavo.hdsbrito@gmail.com>
2026-07-11 21:33:40 -0300

    std  main

--- ftprintf/ft_printf.c
+++ ftprintf/ft_printf.c
@@ -12,12 +12,12 @@
 
 #include "ft_printf.h"
 
-static int	format_handle(char format, va_list args)
-{
-	if (format == 'c')
-		return (ft_print_char(va_arg(args, int)));
+static int	format_handle(char format, va_list args) // format para receber o char depois de % e args para receber uma lista
+{													 // de argumentos variadicos - e extrair os valores com va_arg
+	if (format == 'c')                             // mesmo com a função recebendo um char como parametro, o proprio C faz uma 
+		return (ft_print_char(va_arg(args, int))); // "conversão" de tipo para int
 	else if (format == 's')
-		return (ft_print_string(va_arg(args, char *)));
+		return (ft_print_string(va_arg(args, char *))); 
 	else if (format == 'p')
 		return (ft_print_pointer(va_arg(args, void *)));
 	else if (format == 'd')
@@ -31,28 +31,29 @@ static int	format_handle(char format, va_list args)
 	else if (format == '%')
 		return (ft_print_char('%'));
 	return (0);
+	/* usamos if e else if para verificar qual sera a flag utilizada e consequentemente a função que vai ser chamada*/
 }
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...) // recebe uma string imutavel e uma quantidade variável de argumentos
 {
-	va_list	args;
-	int		count;
-	int		i;
+	va_list	args; // guarda lista de argumentos
+	int		count; // contador de char impressos
+	int		i; // indice (contador) para percorrer format
 
-	va_start(args, format);
+	va_start(args, format); // inicializa args, resumidamente -- para ler os args após o format
 	count = 0;
 	i = 0;
 	while (format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%') // verificar se o i = 0 é %, se for, chama a função auxiliar.
 		{
-			count += format_handle(format[i + 1], args);
-			i++;
+			count += format_handle(format[i + 1], args); // count recebe o resultado da função (que todas as funções retornam basicamente o size da impressão)
+			i++;                                         // e chama a format com o parametro seguinte de %  -- no caso o i + 1 e passa tambem os args
 		}
 		else
-			count += ft_print_char(format[i]);
+			count += ft_print_char(format[i]); // se a o if acima não acontecer, count recebe o valor da função chamada e printamos somente o %
 		i++;
 	}
-	va_end(args);
-	return (count);
+	va_end(args); // encerramos o va_start e o uso dos argumentos variadicos
+	return (count); // retornamos o total de count  -- total de char impressos
 }
